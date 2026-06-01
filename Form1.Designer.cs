0e140cb1d860fb32ad60230b9e67fd632abef244
francistuar-ops <francistuar@gmail.com>
2026-05-31 23:57:56 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Sistemapubs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(257, 42);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Text = "txtusuario";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(257, 84);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.Text = "txtclave\r\n";
+            this.textBox4.UseSystemPasswordChar = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(169, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ingresar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(259, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 32);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Salir";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(144, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Contraseña";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(450, 244);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Sistemapubs
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection(
+        @"Data Source=(localdb)\MSSQLLocalDB;
+        Initial Catalog=Sistemapubs;
+        Integrated Security=True");
+
+        int intentos = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = textBox1.Text;
+            string clave = textBox2.Text;
+
+            if (usuario == "admin" && clave == "123")
+            {
+                MessageBox.Show("Bienvenido al sistema");
+            }
+            else
+            {
+                intentos--;
+
+                MessageBox.Show("Usuario o contraseña incorrectos\nIntentos restantes: " + intentos);
+
+                if (intentos <= 0)
+                {
+                    MessageBox.Show("Se acabaron los intentos permitidos");
+                    Application.Exit();
+                }
+
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "admin" &&
+    txtClave.Text == "123")
+            {
+                MessageBox.Show("Bienvenido");
+            }
+            else
+            {
+                intentos++;
+
+                MessageBox.Show(
+                    "Usuario o contraseña incorrectos\nIntento "
+                    + intentos + " de 3");
+
+                if (intentos >= 3)
+                {
+                    MessageBox.Show(
+                        "Ha superado los 3 intentos permitidos");
+
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
